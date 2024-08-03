e00f71f4f6221e253e919fed09a43503b1e70847
Andres-A-Cedeno <aacedenob@puce.edu.ec>
2024-08-03 10:08:31 -0500

    fix: cambios en supabaseClient

--- integrador/backend/config/supabaseClient.js
+++ integrador/backend/config/supabaseClient.js
@@ -4,9 +4,6 @@ import dotenv from "dotenv";
 // Cargar variables de entorno
 dotenv.config();
 
-console.log("SUPABASE_URL:", process.env.SUPABASE_URL); // Log para verificar
-console.log("SUPABASE_KEY:", process.env.SUPABASE_KEY); // Log para verificar
-
 // Configuracion de credenciales de Supabase
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
