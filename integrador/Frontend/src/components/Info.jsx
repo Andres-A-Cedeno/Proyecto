6fd7b7aebfa1ba9889d285d86de130d5f306de93
Andrés Cedeño <168155877+Andres-A-Cedeno@users.noreply.github.com>
2024-08-14 19:21:58 -0500

    feat: Responsive design

--- integrador/Frontend/src/components/Info.jsx
+++ integrador/Frontend/src/components/Info.jsx
@@ -1,18 +1,17 @@
-//import { useLocation } from "react-router-dom";
 import Buttons from "./Buttons.jsx";
-//import { Link } from "react-router-dom";
 
 function Info() {
-  //const location = useLocation();
-  //const isLandingPage = location.pathname === "/";
-  //{isLandingPage && <Buttons />}
   return (
-    <div className="flex flex-col bg-white justify-center items-center p-20 gap-2 w-1/2 min-h-screen text-black">
-      <img src="/logo.png" alt="logo" className="w-28" />
-      <div className="">
-        <h1 className="font-bold text-3xl">Relajate jugando,</h1>
-        <h1 className="font-bold text-3xl mb-4">Organizate ganando</h1>
-        <p className="mb-4">
+    <div className="flex flex-col bg-white justify-center items-center p-6 sm:p-10 md:p-20 gap-4 w-full sm:w-3/4 md:w-1/2 rounded-lg text-black mx-4 mx-auto">
+      <img src="/logo.png" alt="logo" className="w-16 sm:w-24 md:w-28 mb-4" />
+      <div className="text-center">
+        <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl">
+          Relájate jugando,
+        </h1>
+        <h1 className="font-bold text-2xl sm:text-3xl md:text-4xl mb-4">
+          Organízate ganando
+        </h1>
+        <p className="mb-6 text-sm sm:text-base md:text-lg">
           ¡Bienvenido a nuestra página web! Sumérgete en nuestro juego diseñado
           para relajarte y divertirte, ideal para liberar el estrés. Además, te
           ofrecemos herramientas para organizar tus tareas pendientes,
--- integrador/Frontend/src/pages/Home.jsx
+++ integrador/Frontend/src/pages/Home.jsx
@@ -4,7 +4,9 @@ import Info from "../components/Info";
 const Home = () => {
   return (
     <MainLayout title="Inicio">
-      <Info />
+      <div className="flex justify-center items-center bg-violet-700/30 min-h-screen">
+        <Info />
+      </div>
     </MainLayout>
   );
 };
